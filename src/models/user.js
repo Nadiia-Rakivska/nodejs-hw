51e60089043d06668fa29324641821158f47fc3c
Nadiia Rakivska <rakivska94@gmail.com>
2025-12-16 11:15:25 +0200

    create

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,11 @@ const userSchema = new Schema(
     username: { type: String, trim: true, required: false },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true, minlength: 8 },
+    avatar: {
+      type: String,
+      required: false,
+      default: '<https://ac.goit.global/fullstack/react/default-avatar.jpg>',
+    },
   },
   {
     versionKey: false,
@@ -12,17 +17,17 @@ const userSchema = new Schema(
   },
 );
 
-userSchema.pre('save', function (next) {
-  if (!this.username) {
-    this.username = this.email;
-  }
-  next();
-});
-// userSchema.pre('save', function () {
+// userSchema.pre('save', function (next) {
 //   if (!this.username) {
 //     this.username = this.email;
 //   }
+//   next();
 // });
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
 
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
